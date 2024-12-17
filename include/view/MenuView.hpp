6f6da58718b35fcb61168a4591d1d13eb9336a95
crisneon1823 <camilo.riscanevo@correounivalle.edu.co>
2024-12-17 10:42:45 -0500

    primer menu

---
+++ include/view/MenuView.hpp
@@ -0,0 +1,9 @@
+#pragma once 
+#include <iostream>
+
+using namespace std; 
+
+class MenuView{
+    public:
+    void showMenu();
+};
---
+++ src/view_implementation/MenuView.cpp
@@ -0,0 +1,36 @@
+#include "MenuView.hpp"
+#include <iostream>
+#include <string>
+using namespace std; 
+
+void MenuView::showMenu() {
+    int opcion; 
+    cout << "1. Cliente" << endl; 
+    cout << "2. Empleado" << endl; 
+    cout << "3. Administrador" << endl; 
+    cout << "5. salir" << endl; 
+    cout<<"_"<<endl;
+    cout << "Elija su opcion: " << endl; cin>>  opcion; 
+    cout<<"_"<<endl;
+    switch (opcion) {
+        case 1: {
+            cout << "Menu cliente" << endl;
+            break; 
+        }
+        case 2: {
+            cout << "Menu empleado" << endl;
+            break; 
+        }
+        case 3: {
+            cout << "Menu administrador" << endl;
+            break; 
+        }
+        case 5: {
+            cout << "Saliendo del sistema" << endl;
+            break; 
+        }
+        default: {
+            cout << "Elija una opcion correcta mlp" << endl;
+            break; 
+        }
+}
